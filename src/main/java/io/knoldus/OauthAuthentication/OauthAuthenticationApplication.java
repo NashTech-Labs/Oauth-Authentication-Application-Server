377dacb50bd338e5d3367a9b635f23e6a38d98f1
ankit-mogha <ankit.mogha@knoldus.com>
2022-07-28 15:04:58 +0530

    Initial commit

---
+++ src/main/java/io/knoldus/OauthAuthentication/OauthAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package io.knoldus.OauthAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/knoldus/OauthAuthentication/OauthAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.knoldus.OauthAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
